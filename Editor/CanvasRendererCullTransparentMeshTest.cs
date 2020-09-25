e053a1dfcd1915fb66b07da6dba968de798cd84d
baba_s <shotababa1206@gmail.com>
2020-09-25 21:15:51 +0900

    first commit

---
+++ Editor/CanvasRendererCullTransparentMeshTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+	internal sealed class CanvasRendererCullTransparentMeshTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void 透明な_UI_の_Cul_Transparent_Mesh_がオンになっているか()
+		{
+			bool IsValid( GameObject gameObject )
+			{
+				var graphic = gameObject.GetComponent<Graphic>();
+				if ( graphic == null ) return true;
+				var canvasRenderer = gameObject.GetComponent<CanvasRenderer>();
+				if ( canvasRenderer == null ) return true;
+				var mask = gameObject.GetComponent<Mask>();
+				if ( mask != null ) return true;
+				return 0 < graphic.color.a || canvasRenderer.cullTransparentMesh;
+			}
+
+			AllGameObjectTester.Test( IsValid );
+		}
+	}
+}
